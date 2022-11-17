a0af3df37ea51807d64beda29501bbc53555d7cb
stefanyzm <stefanyzm1991@hotmail.com>
2022-11-16 23:59:39 -0500

    Se editó layout del perfil para agregar textos editables

---
+++ FORMACION/CERTIFIED TECH DEVELOPER/BIMESTRE 3/POO/PRACTICA/Semana2Clase4S/com/DigitalBank/principal/main.java
@@ -0,0 +1,2 @@
+package com.DigitalBank.principal.com.DigitalBank.principal;public class main {
+}
---
+++ FORMACION/MISION TIC - UNAL/CICLO 4/PRACTICA/app/src/main/java/com/example/verfresh/view/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.verfresh.view.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.verfresh.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
